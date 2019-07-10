78951b83779620facd36cf9e224a85dc9b7f5e25
Chase <chasewilliams98@gmail.com>
2019-07-10 16:37:32 -0400

    Removed unused imports

--- src/main/java/com/im/ItemManagerApplication.java
+++ src/main/java/com/im/ItemManagerApplication.java
@@ -2,7 +2,6 @@ package com.im;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
